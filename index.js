cb7ed48e2eb2bd035fe9c1c9f45d0eb328ca1fe0
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-05-01 18:30:13 -0300

    :sparkle: feat: criacao de middleware para verificacao de token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(require("./routes/userRoutes"))
+app.use(require("./routes/privateRoute"))
 
 // credenciais
 const dbUser = process.env.DB_USER
