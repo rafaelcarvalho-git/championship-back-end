839e61ef0d43d1d67b89c093ecaec87d9b098619
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-05-03 16:45:26 -0300

    :sparkles: feat: adicionando rota de criar jogadores

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 app.use(require("./routes/userRoutes"))
 app.use(require("./routes/privateRoute"))
 app.use(require("./routes/teamMongoose"))
+app.use(require("./routes/playerRoute"))
 
 // credenciais
 const dbUser = process.env.DB_USER
---
+++ server/routes/playerRoute.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const playerModel = require("../models/playerModel")
+
+const routes = express.Router()
+
+routes.post("/player", async (req, res) => {
+
+  // retrieving data from the body and checking if they exist
+  const {name, picture, height, weight, age, position, number, idTeam} = req.body
+
+  if (!name || !picture || !height || !weight || !age || !position || !number || !idTeam) {
+    return res.status(400).json({
+      message: "Dado necessario esta faltando"
+    })
+  }
+
+  // checking if there is more than 22 players in the team
+
+  try {
+
+    const players = await playerModel.find({
+      idTeam: idTeam
+    })
+  
+    if (players.length == 22) {
+      return res.status(400).json({
+        message: "Quantidade de jogadores excedida"
+      })
+    }
+
+  } catch (error) {
+    
+    return res.status(500).json({
+      message: "Erro ao buscar jogadores"
+    })
+
+  }
+
+  try {
+
+    // inserting on the database
+    const player = {
+      name, picture, height, weight, age, position, number, idTeam
+    }
+
+    await playerModel.create(player)
+
+    return res.status(200).json({
+      message: "Jogador criado com sucesso"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Erro ao inserir jogador"
+    })
+  }
+
+})
+
+module.exports = routes
