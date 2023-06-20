f1e16c15db5e2337d64c6006c0b204de11cba834
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-06-19 22:52:59 -0300

    :sparkles: feat: adicionando cors

--- server/index.js
+++ server/index.js
@@ -6,9 +6,6 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 
-app.use(cors())
-
-app.use(cors())
 app.use(express.json())
 app.use(require("./routes/userRoutes"))
 app.use(require("./routes/teamRoute"))
