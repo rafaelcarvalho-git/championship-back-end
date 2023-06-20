8fc401ec73d87c8a3ad73eb05b913413c8652734
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-06-19 22:58:02 -0300

    :sparkles: feat: adicionando cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
-const cors = require("cors")
 
 const app = express()
-app.use(cors())
 
+app.use(require("./middleware/cors"))
 app.use(express.json())
 app.use(require("./routes/userRoutes"))
 app.use(require("./routes/teamRoute"))
